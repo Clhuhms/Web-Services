6380b2e84afb16df62bd3e76a838fb069fd7f671
Clhuhms <36223672+Clhuhms@users.noreply.github.com>
2019-01-24 12:14:37 +0100

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
-//inuts
+//inputs
 const input_xml = document.querySelector('#input_xml');
 const input_xpath = document.querySelector('#input_xpath');
+//const xpath_test = document.querySelector('#xpath_test');
 
 //outputs
 const out_root = document.querySelector('#out_root');
@@ -17,7 +18,29 @@ input_xml.onkeyup = function()
 {
 	xmldoc = parser.parseFromString(this.value, 'text/xml');
 	out_root.innerText = xmldoc.documentElement.tagName
+	out_count.innerText = xmldoc.documentElement.childElementCount
+	out_children.innerText = xmldoc.documentElement.children[0].tagName
+	
+	
 };
+
+input_xpath.onkeyup = function()
+{	
+	const nodes = xmldoc.evaluate(input_xpath, xmldoc, null, XPathResult.ANY_TYPE, null);
+	
+	let result = nodes.iterateNext();
+	let concat = '';
+	
+	while (result) 
+	{
+		const str = new XMLSerializer.serializeToString(result);
+		concat += str;
+		result = nodes.iterateNext();
+	}
+	out_xpath.innerText = concat;
+};
+
+
 
 input_xpath.onkeyup = function()
 {
